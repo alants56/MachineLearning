69ad991040462440314025231b308a004376d483
alants <alants>
2019-03-18 21:54:00 +0800

    modify nonparam

--- nonparam/nonparam.py
+++ nonparam/nonparam.py
@@ -32,13 +32,6 @@ def eudistance(x, y) :
         d += t ** 2
     return (d ** 0.5)
 
-def get_nindex(D, n) :
-    C = copy.copy(D)
-    C.sort()
-    for i in range(len(D)) :
-        if D[i] == C[n-1] :
-            return i
-    return -1
 
 def knn(Data, X, kn, d, f) :
     t = kn / len(Data)
@@ -47,12 +40,12 @@ def knn(Data, X, kn, d, f) :
         dis = []
         for s in Data :
             dis.append(f(x,s))
-        index = get_nindex(dis, kn)
-        v = (f(x,Data[index]) * 2) ** d
+        dis.sort()
+        v = (dis[kn] * 2) ** d
         Prob.append(t/v)
     return np.array(Prob)
 
-
+'''
 
 def test_1D() :
     Data = np.random.normal(0, 1, 10000).reshape([-1,1])
@@ -101,10 +94,254 @@ def test_2D() :
 
     plt.show()
 
+
+def test_1D_Parzen_cube() :
+    Data = np.random.normal(0, 1, 10000).reshape([-1,1])
+    X = np.arange(-5, 5, 0.1).reshape([-1,1])
+    Prob1 = Parzen(Data, X, h = 0.01, d = 1, f = cube)
+
+    fig = plt.figure()
+    fig.suptitle("Parzen")
+
+    ax = plt.subplot(1,3,1)
+    ax.set_title("h = 0.01")
+    ax.plot(X, Prob1)
+
+    Prob2 = Parzen(Data, X, h = 0.1, d = 1, f = cube)
+    ax = plt.subplot(1,3,2)
+    ax.set_title("h = 0.1")
+    ax.plot(X, Prob2)
+
+    Prob3 = Parzen(Data, X, h = 1, d = 1, f = cube)
+    ax = plt.subplot(1,3,3)
+    ax.set_title("h = 1")
+    ax.plot(X, Prob3)
+    plt.show()
+
+def test_1D_Parzen_gaussian() :
+    Data = np.random.normal(0, 1, 10000).reshape([-1,1])
+    X = np.arange(-5, 5, 0.1).reshape([-1,1])
+
+    fig = plt.figure()
+    fig.suptitle("Parzen")
+
+    Prob1 = Parzen(Data, X, h = 0.01, d = 1, f = gaussian_1D)
+    ax = plt.subplot(1,3,1)
+    ax.set_title("h = 0.01")
+    ax.plot(X, Prob1)
+
+    Prob2 = Parzen(Data, X, h =0.1, d = 1, f = gaussian_1D)
+    ax = plt.subplot(1,3,2)
+    ax.set_title("h = 0.1")
+    ax.plot(X, Prob2)
+
+    Prob3 = Parzen(Data, X, h = 1, d = 1, f = gaussian_1D)
+    ax = plt.subplot(1,3,3)
+    ax.set_title("h = 1")
+    ax.plot(X, Prob3)
+    plt.show()
+
+def test_1D_knn() :
+    Data = np.random.normal(0, 1, 10000).reshape([-1,1])
+    X = np.arange(-5, 5, 0.1).reshape([-1,1])
+
+    fig = plt.figure()
+    fig.suptitle("knn")
+
+    Prob1 = knn(Data, X, kn = 10, d = 1, f = eudistance)
+    ax = plt.subplot(1,3,1)
+    ax.set_title("kn = 10")
+    ax.plot(X, Prob1)
+
+    Prob2 = knn(Data, X, kn = 30, d = 1, f = eudistance)
+    ax = plt.subplot(1,3,2)
+    ax.set_title("kn = 30")
+    ax.plot(X, Prob2)
+
+
+    Prob3 = knn(Data, X, kn = 100, d = 1, f = eudistance)
+    ax = plt.subplot(1,3,3)
+    ax.set_title("kn = 100")
+    ax.plot(X, Prob3)
+    plt.show()
+
+
+def test_2D_Parzen() :
+    Data = np.random.randn(1000, 2)
+    x = np.arange(-5, 5, 0.1)
+    y = np.arange(-5, 5, 0.1)
+    n = len(x) 
+    X = []
+    for i in x :
+        for j in y :
+            X.append([i,j])
+    X = np.array(X)
+
+    fig = plt.figure()
+    fig.suptitle("Parzen")
+
+    Prob1 = Parzen(Data, X, h = 0.01, d = 2, f = cube)
+    ax = plt.subplot(1,3,1, projection='3d')
+    ax.set_title("h = 0.01")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob1, cmap='rainbow')
+   
+    Prob2 = Parzen(Data, X, h = 0.1, d = 2, f = cube)
+    ax = plt.subplot(1,3,2, projection='3d')
+    ax.set_title("h = 0.1")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob2, cmap='rainbow')
+
+    Prob3 = Parzen(Data, X, h = 1, d = 2, f = cube)
+    ax = plt.subplot(1,3,3, projection='3d')
+    ax.set_title("h = 1")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob2, cmap='rainbow')
+
+    plt.show()
+
+
+def test_2D_knn() :
+    Data = np.random.randn(1000, 2)
+    x = np.arange(-5, 5, 0.1)
+    y = np.arange(-5, 5, 0.1)
+    n = len(x) 
+    X = []
+    for i in x :
+        for j in y :
+            X.append([i,j])
+    X = np.array(X)
+
+    fig = plt.figure()
+    fig.suptitle("knn")
+
+    Prob1 = knn(Data, X, kn = 10, d = 2, f = eudistance)
+    ax = plt.subplot(1,3,1, projection='3d')
+    ax.set_title("kn = 10")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob1, cmap='rainbow')
+   
+    Prob2 = knn(Data, X, kn = 40, d = 2, f = eudistance)
+    ax = plt.subplot(1,3,2, projection='3d')
+    ax.set_title("kn = 40")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob2, cmap='rainbow')
+
+    Prob3 = knn(Data, X, kn = 100, d = 2, f = eudistance)
+    ax = plt.subplot(1,3,3, projection='3d')
+    ax.set_title("kn = 100")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob3, cmap='rainbow')
+    plt.show()
+'''
+
+def test_1D_pk() :
+    Data = np.random.normal(0, 1, 10000).reshape([-1,1])
+    X = np.arange(-5, 5, 0.1).reshape([-1,1])
+    Prob1 = Parzen(Data, X, h = 0.01, d = 1, f = cube)
+
+
+
+    ax = plt.subplot(3,3,1)
+    ax.set_title("h = 0.01")
+    ax.plot(X, Prob1)
+    plt.ylabel("Parzen: cube")
+
+    Prob2 = Parzen(Data, X, h = 0.1, d = 1, f = cube)
+    ax = plt.subplot(3,3,2)
+    ax.set_title("h = 0.1")
+    ax.plot(X, Prob2)
+
+    Prob3 = Parzen(Data, X, h = 1, d = 1, f = cube)
+    ax = plt.subplot(3,3,3)
+    ax.set_title("h = 1")
+    ax.plot(X, Prob3)
+
+
+
+    Prob1 = Parzen(Data, X, h = 0.01, d = 1, f = gaussian_1D)
+    ax = plt.subplot(3,3,4)
+    ax.set_title("h = 0.01")
+    ax.plot(X, Prob1)
+    plt.ylabel("Parzen: gaussian")
+
+    Prob2 = Parzen(Data, X, h =0.1, d = 1, f = gaussian_1D)
+    ax = plt.subplot(3,3,5)
+    ax.set_title("h = 0.1")
+    ax.plot(X, Prob2)
+
+    Prob3 = Parzen(Data, X, h = 1, d = 1, f = gaussian_1D)
+    ax = plt.subplot(3,3,6)
+    ax.set_title("h = 1")
+    ax.plot(X, Prob3)
+
+
+
+    Prob1 = knn(Data, X, kn = 10, d = 1, f = eudistance)
+    ax = plt.subplot(3,3,7)
+    ax.set_title("kn = 10")
+    ax.plot(X, Prob1)
+
+    plt.ylabel("knn")
+
+    Prob2 = knn(Data, X, kn = 30, d = 1, f = eudistance)
+    ax = plt.subplot(3,3,8)
+    ax.set_title("kn = 30")
+    ax.plot(X, Prob2)
+
+
+    Prob3 = knn(Data, X, kn = 100, d = 1, f = eudistance)
+    ax = plt.subplot(3,3,9)
+    ax.set_title("kn = 100")
+    ax.plot(X, Prob3)
+    plt.show()
+
+
+def test_2D_pk() :  
+    Data = np.random.randn(1000, 2)
+    x = np.arange(-5, 5, 0.1)
+    y = np.arange(-5, 5, 0.1)
+    n = len(x) 
+    X = []
+    for i in x :
+        for j in y :
+            X.append([i,j])
+    X = np.array(X)
+
+
+    Prob1 = Parzen(Data, X, h = 0.02, d = 2, f = cube)
+    ax = plt.subplot(2,3,1, projection='3d')
+    ax.set_title("Parzen: h = 0.02")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob1, cmap='rainbow')
+
+
+    Prob2 = Parzen(Data, X, h = 0.2, d = 2, f = cube)
+    ax = plt.subplot(2,3,2, projection='3d')
+    ax.set_title("Parzen: h = 0.2")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob2, cmap='rainbow')
+
+    Prob3 = Parzen(Data, X, h = 2, d = 2, f = cube)
+    ax = plt.subplot(2,3,3, projection='3d')
+    ax.set_title("Parzen: h = 2")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob3, cmap='rainbow')
+
+
+
+
+    Prob1 = knn(Data, X, kn = 10, d = 2, f = eudistance)
+    ax = plt.subplot(2,3,4, projection='3d')
+    ax.set_title("knn: kn = 10")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob1, cmap='rainbow')
+   
+
+    Prob2 = knn(Data, X, kn = 40, d = 2, f = eudistance)
+    ax = plt.subplot(2,3,5, projection='3d')
+    ax.set_title("knn: kn = 40")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob2, cmap='rainbow')
+
+    Prob3 = knn(Data, X, kn = 100, d = 2, f = eudistance)
+    ax = plt.subplot(2,3,6, projection='3d')
+    ax.set_title("knn: kn = 100")
+    ax.plot_trisurf(X[:,0], X[:,1], Prob3, cmap='rainbow')
+    plt.show()
+
 def main() :
-    test_1D()
-    test_2D()
-    
+    test_1D_pk()
+    test_2D_pk()
 
 if __name__ == '__main__':
     main()
